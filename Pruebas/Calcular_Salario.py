3ca290bc6048723fb75a6fa3cdf598e1a9a0511a
fryntiz <fryntiz@gmail.com>
2017-05-29 19:55:11 +0200

    Completado script

--- Pruebas/Calcular_Salario.py
+++ Pruebas/Calcular_Salario.py
@@ -5,3 +5,8 @@
 # Calcula el salario pidiendo número de horas y tarifa
 # ==============================================================================
 
+horas = raw_input('Introduce el número de horas: ')
+tarifa = raw_input('Introduce la tarifa por hora: ')
+salario = int(horas) * int(tarifa)
+
+print(salario)
