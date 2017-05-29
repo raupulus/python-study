74849bc7464be8b2c7f99774eb95dd445623c713
fryntiz <fryntiz@gmail.com>
2017-05-29 20:04:02 +0200

    Conversor de C a F terminado

--- Pruebas/De_GradosC_a_GradosF.py
+++ Pruebas/De_GradosC_a_GradosF.py
@@ -5,3 +5,7 @@
 # Conversor de grados Centígrados a grados F.
 # ==============================================================================
 
+gradosC = raw_input('Introduce los grados centígrados --> ')
+gradosF = (float(gradosC) * 9 / 5) + 32
+
+print('Equivale a %sºF (grados Fahrenheit)' % gradosF)
