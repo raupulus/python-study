ede6a39c4e99f08ddb9d06f28f8964bb06c9784f
fryntiz <fryntiz@gmail.com>
2017-06-04 03:12:43 +0200

    a

--- Pruebas/Contar_Palabras.py
+++ Pruebas/Contar_Palabras.py
@@ -3,3 +3,12 @@
 # ==============================================================================
 # 				Pedir archivo y contar Palabras
 # ==============================================================================
+nombref = raw_input('Introduzca el nombre del fichero: ')
+
+try:
+	manf = open(nombredef)
+except:
+	print '\nEl archivo %s no se pudo abrir' % nombref
+	exit()
+
+
