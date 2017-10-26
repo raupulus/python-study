79296f906a47a12bfa74e421dc77f070672467b4
fryntiz <fryntiz@gmail.com>
2017-10-26 10:42:04 +0200

    Añadido objetivo del script

--- Internet/Info_IP.py
+++ Internet/Info_IP.py
@@ -9,6 +9,9 @@
 # ###       www.fryntiz.es        ### #
 #######################################
 
+# Función para obtener información sobre una IP pasada como parámetro
+# En caso de no recibir una IP tomará nuestra IP pública
+# Se depende del paquete "geoiplookup" en el sistema operativo
 
 ##############################
 ##         LIBRERÍAS        ##
