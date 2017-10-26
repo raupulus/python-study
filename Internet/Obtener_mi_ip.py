76010a6560332964149114019e1c279a4efe9795
fryntiz <fryntiz@gmail.com>
2017-10-26 02:54:24 +0200

    Creado script para obtener la IP

--- Internet/Obtener_mi_ip.py
+++ Internet/Obtener_mi_ip.py
@@ -12,5 +12,24 @@
 # Este script obtiene la dirección IP del equipo que la ejecuta
 
 ##############################
-##         Variables        ##
+##         LIBRERÍAS        ##
 ##############################
+import urllib2  # Librería para obtener web
+
+def mi_ip():
+    # Variable donde guardar la dirección IP
+    ip = ''
+
+    # Añadir web html a variable
+    web = urllib2.urlopen('http://checkip.dyndns.org').read()
+
+    # Recorrer html descargado en busca de números que conforman la ip
+    for line in str(web):
+        # Extraer números y puntos separadores
+        if line in str(range(10)) + '.':
+            ip += line
+
+    print ('La ip es → ' + ip.strip())
+    return ip.strip()
+
+print(mi_ip())
